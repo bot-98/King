b3ac17e104da881f27e1b0bf7d288ae5e671e67f
bot-98 <bbzrihan93@gmail.com>
2025-08-19 05:19:08 +0530

    Update say.js

--- Script/commands/say.js
+++ Script/commands/say.js
@@ -1,27 +1,40 @@
 module.exports.config = {
-	name: "say",
-	version: "1.0.1",
-	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-	description: "Make the bot return google's audio file via text",
-	commandCategory: "media",
-	usages: "[ru/en/ko/ja/tl] [Text]",
-	cooldowns: 5,
-	dependencies: {
-		"path": "",
-		"fs-extra": ""
-	}
+ 'name': 'say',
+ 'version': "1.0.1",
+ 'hasPermssion': 0x0,
+ 'credits': "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ 'description': "Make the bot return google's audio file via text",
+ 'commandCategory': "media",
+ 'usages': "[bn] [Text]",
+ 'cooldowns': 0x5,
+ 'dependencies': {
+ 'path': '',
+ 'fs-extra': ''
+ }
 };
-
-module.exports.run = async function({ api, event, args }) {
-	try {
-		const { createReadStream, unlinkSync } = global.nodemodule["fs-extra"];
-		const { resolve } = global.nodemodule["path"];
-		var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
-		var languageToSay = (["ru","en","pr","ja", "tl", "bn"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
-		var msg = (languageToSay != global.config.language) ? content.slice(3, content.length) : content;
-		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
-		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=${languageToSay}&client=tw-ob`, path);
-		return api.sendMessage({ attachment: createReadStream(path)}, event.threadID, () => unlinkSync(path), event.messageID);
-	} catch (e) { return console.log(e) };
-}
+module.exports.run = async function ({
+ api: _0x5cee6f,
+ event: _0x6b558b,
+ args: _0x5c8dc1
+}) {
+ try {
+ const {
+ createReadStream: _0x19316d,
+ unlinkSync: _0x1b899a
+ } = global.nodemodule["fs-extra"];
+ const {
+ resolve: _0x1b6f6e
+ } = global.nodemodule.path;
+ var _0x50a304 = _0x6b558b.type == "message_reply" ? _0x6b558b.messageReply.body : _0x5c8dc1.join(" ");
+ var _0x173460 = ['bn'].some(_0x657a78 => _0x50a304.indexOf(_0x657a78) == 0) ? _0x50a304.slice(0, _0x50a304.indexOf(" ")) : global.config.language;
+ var _0xa243c2 = _0x173460 != global.config.language ? _0x50a304.slice(3, _0x50a304.length) : _0x50a304;
+ const _0x5e6854 = _0x1b6f6e(__dirname, "cache", _0x6b558b.threadID + '_' + _0x6b558b.senderID + ".mp3");
+ await global.utils.downloadFile("https://translate.google.com/translate_tts?ie=UTF-8&q=" + encodeURIComponent(_0xa243c2) + "&tl=bn&client=tw-ob", _0x5e6854);
+ return _0x5cee6f.sendMessage({
+ 'attachment': _0x19316d(_0x5e6854)
+ }, _0x6b558b.threadID, () => _0x1b899a(_0x5e6854));
+ } catch (_0x1008a6) {
+ return console.log(_0x1008a6);
+ }
+ ;
+};
