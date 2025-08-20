94873dbea068dcec82a3608b054a16539efc127b
bot-98 <bbzrihan93@gmail.com>
2025-08-20 06:40:07 +0530

    Update emojivoice.js

--- Script/commands/emojivoice.js
+++ Script/commands/emojivoice.js
@@ -17,8 +17,8 @@ const emojiAudioMap = {
  "🥱": "https://files.catbox.moe/9pou40.mp3",
  "😁": "https://files.catbox.moe/60cwcg.mp3",
  "🙄": "https://files.catbox.moe/2e059f.mp4",
- "🥲": "https://files.catbox.moe/4fq3gv.bin"'
- "🙂": "https://files.catbox.moe/srnc7q.mp3",
+ "🤦‍♂️": "https://files.catbox.moe/snjqqy.mp4",
+ "🙂": "https://files.catbox.moe/r93evz.bin",
  "😌": "https://files.catbox.moe/epqwbx.mp3",
  "🥺": "https://files.catbox.moe/wc17iq.mp3",
  "🤭": "https://files.catbox.moe/cu0mpy.mp3",
