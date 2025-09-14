e2446c93d4d98a85802d864781ce7789c3488cfc
bot-98 <bbzrihan93@gmail.com>
2025-09-15 03:13:18 +0530

    Update war.js

--- Script/commands/war.js
+++ Script/commands/war.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-    name: "xud",
+    name: "murgi2",
     version: "1.0.0",
     hasPermssion: 2,
     credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
@@ -15,31 +15,31 @@ module.exports.config = {
 
 module.exports.run = async function({ api, args, Users, event}) {
     var mention = Object.keys(event.mentions)[0];
-    if(!mention) return api.sendMessage("বস যে মাগির পোলা ক্সুদ্দে চাউ সে ওই মাল্টা @ম্যানশন দেউ", event.threadID);
+    if(!mention) return api.sendMessage("বস যে হাত মারা মাগিরে চুদ-তে চাউ @ম্যানশন দেউ", event.threadID);
  let name =  event.mentions[mention];
     var arraytag = [];
         arraytag.push({id: mention, tag: name});
     var a = function (a) { api.sendMessage(a, event.threadID); }
-a("চুদা লো");
-setTimeout(() => {a({body: "খাংকির ছেলে তর মারে চুদি 🥰।" + " " + name, mentions: arraytag})}, 3000);
-setTimeout(() => {a({body: "খাংকির ছেলে তর কচি বোন রে চুদি 😍.." + " " + name, mentions: arraytag})}, 5000);
-setTimeout(() => {a({body: "মাদারচোদ তর আম্মু পম পম খাংকির পো 🐰" + " " + name, mentions: arraytag})}, 7000);
-setTimeout(() => {a({body: "খাংকির ছেলে তর কচি ভুদায় ভুদায় কামর দিমু  💔!" + " " + name, mentions: arraytag})}, 9000);
-setTimeout(() => {a({body: "খাংকি মাগির ছেলে কথা ক কম কম তর আম্মু রে চুদে বানামু আইটেম বোম " + " " + name, mentions: arraytag})}, 12000);
+a("চুদা লো এইবার সোসা মারা মাগী");
+setTimeout(() => {a({body: "খাংকির মেয়ে তর মারে চুদি 🥰।" + " " + name, mentions: arraytag})}, 3000);
+setTimeout(() => {a({body: "খাংকির মেয়ে তর কচি বোন রে চুদি 😍.." + " " + name, mentions: arraytag})}, 5000);
+setTimeout(() => {a({body: "মাদারচোদ তর আম্মু পম পম খাংকির মেয়ে 🐰" + " " + name, mentions: arraytag})}, 7000);
+setTimeout(() => {a({body: "খাংকির মেয়ে তর কচি ভুদায় ভুদায় কামর দিমু  💔!" + " " + name, mentions: arraytag})}, 9000);
+setTimeout(() => {a({body: "খাংকি মাগির মেয়ে কথা ক কম কম তর আম্মু রে চুদে বানামু আইটেম বোম " + " " + name, mentions: arraytag})}, 12000);
 setTimeout(() => {a({body: "depression থেকেও তর মাইরে চু*** দি 🤬 " + " " + name, mentions: arraytag})}, 15000);
-setTimeout(() => {a({body: "তর আম্মু রে আচার এর লোভ দেখি চুদি মাগির ছেলে🤬" + " " + name, mentions: arraytag})}, 17000);
+setTimeout(() => {a({body: "তর আম্মু রে আচার এর লোভ দেখি চুদি মাগির মেয়ে 🤬" + " " + name, mentions: arraytag})}, 17000);
 setTimeout(() => {a({body: "বান্দির পুত তর কচি বোনের ভুদা ফাক কর থুতু দিয়ে ভুদায় দন ডুকামু 🤟" + " " + name, mentions: arraytag})}, 20000);
-setTimeout(() => {a({body: "বান্দি মাগির পোলা তর আম্মু রে চুদি তর দুলা ভাই এর কান্দে ফেলে  🤝" + " " + name, mentions: arraytag})},23000);
+setTimeout(() => {a({body: "বান্দি মাগির মেয়ে তর আম্মু রে চুদি তর দুলা ভাই এর কান্দে ফেলে  🤝" + " " + name, mentions: arraytag})},23000);
 setTimeout(() => {a({body: "উফফফ খাদ্দামা মাগির সন্তান তর আম্মুর কালা ভুদায় আমার মাল আউট তর কচি বোন রে উপ্তা করে এবার চুদবো  💉।" + " " + name, mentions: arraytag})}, 25000);
 setTimeout(() => {a({body: "অনলাইনে গালি বাজ হয়ে গেছত মাগির মেয়ে এমন চুদা দিমু লাইফ টাইম মনে রাখভি  নয়ন তর বাপ মাগির মেয়ে 😘।" + " " + name, mentions: arraytag})}, 28500);
-setTimeout(() => {a({body: "বাতিজা শুন তর আম্মু রে চুদলে রাগ করবি না তো আচ্ছা জা রাগ করিস না তর আম্মুর কালা ভুদায় আর চুদলাম না তো বোন এর জামা টা খুলে দে  ✋" + " " + name, mentions: arraytag})},31000);
+setTimeout(() => {a({body: "বাতিজি শুন তর আম্মু রে চুদলে রাগ করবি না তো আচ্ছা জা রাগ করিস না তর আম্মুর কালা ভুদায় আর চুদলাম না তো বোন এর জামা টা খুলে দে  ✋" + " " + name, mentions: arraytag})},31000);
 setTimeout(() => {a({body: " হাই মাদারচোদ তর তর ব্যাশা জাতের আম্মু টা রে আদর করে করে চুদি " + " " + name, mentions: arraytag})}, 36000);
 setTimeout(() => {a("~ চুদা কি আরো খাবি মাগির পোল 🤖")} , 39000);
 setTimeout(() => {a({body: "খাংকির মেয়ে 🥰।" + " " + name, mentions: arraytag})}, 42000);
 setTimeout(() => {a({body: "মাদারচোদ😍.." + " " + name, mentions: arraytag})}, 48000);
 setTimeout(() => {a({body: "ব্যাস্যার মেয়ে 🐰" + " " + name, mentions: arraytag})}, 51000);
 setTimeout(() => {a({body: "ব্যাশ্যা মাগির মেয়ে  💔!" + " " + name, mentions: arraytag})}, 54000);
-setTimeout(() => {a({body: "পতিতা মাগির মেয়ে " + " " + name, mentions: arraytag})}, 57000);
+setTimeout(() => {a({body: "পতিতা মাগির মেয়ে🫶 " + " " + name, mentions: arraytag})}, 57000);
 setTimeout(() => {a({body: "depression থেকেও তর মাইরে চু*** দি 🤬 " + " " + name, mentions: arraytag})}, 59400);
 setTimeout(() => {a({body: "তর মারে চুদি" + " " + name, mentions: arraytag})}, 63000);
 setTimeout(() => {a({body: "নাট বল্টু মাগির মেয়ে🤟" + " " + name, mentions: arraytag})}, 66000);
@@ -48,7 +48,7 @@ setTimeout(() => {a({body: "উম্মম্মা তর বোন এরক�
 setTimeout(() => {a({body: "DNA টেষ্ট করা দেখবি আমার চুদা তেই তর জন্ম।" + " " + name, mentions: arraytag})}, 75000);
 setTimeout(() => {a({body: "কামলা মাগির মেয়ে  ✋" + " " + name, mentions: arraytag})},81000);
 setTimeout(() => {a({body: " বাস্ট্রাড এর বাচ্ছা বস্তির মেয়ে " + " " + name, mentions: arraytag})}, 87000);
-setTimeout(() => {a("~ আমার জারজ শন্তান🤖")} , 93000);
+setTimeout(() => {a({body: "~ আমার জারজ শন্তান🤖")} , 93000);
 setTimeout(() => {a({body: "Welcome মাগির মেয়ে 🥰।" + " " + name, mentions: arraytag})}, 99000);
 setTimeout(() => {a({body: "তর কচি বোন এর পম পম😍.." + " " + name, mentions: arraytag})}, 105000);
 setTimeout(() => {a({body: "ব্যাস্যার মেয়ে কথা শুন তর আম্মু রে চুদি গামছা পেচিয়ে🐰" + " " + name, mentions: arraytag})}, 111000);
