ad44a11cb45d86d4f2960ca9c07f37e6681bfbf9
bot-98 <bbzrihan93@gmail.com>
2025-09-15 04:40:50 +0530

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -32,7 +32,7 @@ module.exports.run = async function({ api, event }) {
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🎭 ɪɴsᴛᴀɢʀᴀᴍ : ɴᴀᴢʙᴜʟ-7
 ┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : 𝐍𝐀𝐈 𝐁𝐀𝐋
-┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/nazbul.bbu.143.7
+┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/ITZ.NAJMUL.YOUR.DAD
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞:  ${time}
 ┗━━━━━━━━━━━━━━━━━━━━━┛
@@ -40,7 +40,7 @@ module.exports.run = async function({ api, event }) {
         attachment: fs.createReadStream(__dirname + "/cache/1.png")
     }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
   
-    return request(encodeURI(`https://graph.facebook.com/100065264163407/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
+    return request(encodeURI(`https://graph.facebook.com/61553731125380/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
         .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
         .on('close', () => callback());
 };
